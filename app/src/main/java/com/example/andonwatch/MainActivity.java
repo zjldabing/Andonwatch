10188387e5d61b1e57e6154c39d47f8fedbc24ef
liujingbing <zjldabing@163.com>
2019-08-09 16:42:49 +0800

    add all files

---
+++ app/src/main/java/com/example/andonwatch/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.andonwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println(“新写的东西”);
+		System.out.println(“新写的东西3”);
+    }
+}
